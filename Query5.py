2b76df76c48a6b97163d9f30d33f27a81194311a
Kevin Jacobson <keebinie@mailbolt.com>
2018-11-29 22:55:16 -0800

    Query 5 - Triangles done

--- Query5.py
+++ Query5.py
@@ -9,7 +9,7 @@ View for person:
 
 function (doc) {
   if(doc["Works Written"].length > 1) {
-    emit(doc.Name, doc["Works Written"]);
+    emit(doc["_id"], doc["Works Written"]);
   }
 }
 url:
@@ -20,7 +20,7 @@ View for publication:
 
 function (doc) {
   if(doc["Authors"].length > 1) {
-    emit(doc.Name, doc["Authors"]);
+    emit(doc["_id"], doc["Authors"]);
   }
 }
 url:
@@ -55,17 +55,20 @@ for p in publications:
 
 
 print("Begin hunting for triangles...")
-#Triangle Query (id, count)
-authmost = ('', 0)
+authmost = ''
+authmost_cnt = 0
 step = 0
 for p in authors:
 
     #author under test
     curauth = p.id
     candidates = []
-    step = step + 1
-    print("step: " + str(step))
-    
+   
+    #hueristic based on previous runs. Once someone gets into the hundreds of triangles, very tough to overtake.
+    #if they are not authors of many papers, basically not possible to get beyond them.
+    if (len(p.value) * 2)^2 < authmost_cnt:
+        continue
+ 
     #go through their papers
     for a in p.value:
         #does it also have more than one author?
@@ -77,49 +80,45 @@ for p in authors:
                 if s != curauth and s in authdict:
                     candidates.append((a,s))
                 
-            #print(str(candidates))
-            #input()
-
-    count = 0                
+    count = 0
+    seen = set()
     for c in candidates:
         if len(candidates[1:]) > 0:
             for e in candidates[1:]:
                 #exclude if authors or papers happen to be the same.
                 #example: someone could co-author more than once with another author
                 if c[0] != e[0] and c[1] != e[1]:
-                    #intersection of papers
+                    #intersection of author's papers
                     cset = set(authdict[c[1]]) & set(authdict[e[1]])
+                    #discard current papers
                     cset.discard(c[0])
                     cset.discard(e[0])
+                    #anything left? that's a possible triangle!
                     if len(cset) > 0:
-                        #triangle! How many?
-                        print("TRIANGLE: " + curauth + "-->" + c[0] + "-->" + c[1] + "-->" + cset.pop() + "-->" + e[1] + "-->" + e[0] + "-->" + curauth)
-                        input()
-                        count = count + 1
-                        
-                    
-                    
-    if count > authmost[1]:
-        print("Author " + curauth + " has " + str(count) + " triangles, which is more than " + authmost[0] + " who has " + str(authmost[1]))
-        authmost[0] = curauth
-        authmost[1] = count 
+                        #the pop() funtion on a set is non-deterministic, there will be slight variations on runs.
+                        tmp = set([ c[0], c[1], cset.pop(), e[1], e[0] ])
+                        #check to ensure this triangle is fully unique
+                        if (tmp <= seen) == False:
+                            seen.update(tmp)
+                            count = count + 1
+    #who has the most?                 
+    if count > authmost_cnt:
+        print("Author " + curauth + " has " + str(count) + " triangles, which is more than " + authmost + " who has " + str(authmost_cnt))
+        authmost = curauth
+        authmost_cnt = count 
 
 
-#let's try a mango query
+#mango query to find the one with the most triangles
 mango_query = {
    "selector": {
       "_id": {
-         "$eq": 0
+         "$eq": authmost
       }
    }
 } 
 
 #view the results.
-for i in db.find(mango_query):
-    print(i['Title'])
-
-#for p in authors:
-#    curauth = p.key
-#    print(str(p.row))
+for i in person.find(mango_query):
+    print(i['Name'])
 
 
