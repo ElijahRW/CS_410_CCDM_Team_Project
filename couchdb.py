4808ca2db02934f7005e5f92537678012fce2422
Tim Pugh <timpugh@pdx.edu>
2018-12-05 21:20:32 -0800

    Add files via upload
    couchdb.py and mcecs_picture_rc.py files for gui prototype. Place them both in the same folder and run couchdb.py. Works on python 3.5. (possibly python 3.7, this is untested as the directions for pyqt5 called for python 3.5) The queries work on python 2.7. It will be easier to convert the queries to python 3.5 to adapt them to work together. Right now, the button clicks do nothing as this is only a prototype!

---
+++ couchdb.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'couch-db-gui-prototype.ui'
+#
+# Created by: PyQt5 UI code generator 5.6
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(340, 180, 191, 23))
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_2.setGeometry(QtCore.QRect(260, 250, 381, 23))
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(470, 390, 331, 161))
+        self.label.setObjectName("label")
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(0, 0, 191, 41))
+        self.label_2.setObjectName("label_2")
+        self.label_3 = QtWidgets.QLabel(self.centralwidget)
+        self.label_3.setGeometry(QtCore.QRect(10, 30, 61, 31))
+        self.label_3.setObjectName("label_3")
+        self.label_4 = QtWidgets.QLabel(self.centralwidget)
+        self.label_4.setGeometry(QtCore.QRect(0, 50, 451, 41))
+        self.label_4.setObjectName("label_4")
+        self.label_5 = QtWidgets.QLabel(self.centralwidget)
+        self.label_5.setGeometry(QtCore.QRect(0, 90, 491, 16))
+        self.label_5.setObjectName("label_5")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 21))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "Execute Query 2: Level 3 Co-Authors"))
+        self.pushButton_2.setText(_translate("MainWindow", "Execute Query 3: Co Author Distance for Moshe Vardi from Michael J Franklin"))
+        self.label.setText(_translate("MainWindow", "<html><head/><body><p><img src=\":/Mcecs_Picture/download.jpg\"/></p></body></html>"))
+        self.label_2.setText(_translate("MainWindow", "Cloud and Cluster Database Management"))
+        self.label_3.setText(_translate("MainWindow", "Final Project"))
+        self.label_4.setText(_translate("MainWindow", "Presented by: Kevin Jacobson, Elijah Rich-Wimmer, Jason Larson, Scot Lambert and Tim Pugh"))
+        self.label_5.setText(_translate("MainWindow", "This is an application which executes queries on a CouchDB instance hosted on Google Cloud Platform"))
+
+import mcecs_picture_rc
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
+
